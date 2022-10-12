602d3adbacedd837b0ea967a73f31bae35534804
Jay Patel <jay734883@gmail.com>
2022-10-12 12:45:47 +0530

    fixed exit animation

--- src/component/ShowItem.js
+++ src/component/ShowItem.js
@@ -2,27 +2,25 @@ import React from "react";
 import { motion, AnimatePresence } from "framer-motion";
 
 const ShowItem = ({ items, editItem, deleteItem }) => {
-
     return (
         <>
-            <AnimatePresence>
-                <motion.div
-                    className="showItems"
-                    initial={{ x: "100vw" }}
-                    animate={{ x: 0 }}
-                    transition={{ delay: 2, type: "spring", stiffness: 150 }}
-                    exit={{ x: "-100vw"}}
-                >
+            <motion.div
+                className="showItems"
+                initial={{ x: "100vw" }}
+                animate={{ x: 0 }}
+                transition={{ delay: 2, type: "spring", stiffness: 150 }}
+                exit={{ x: "-100vw" }}
+            >
+                <AnimatePresence>
                     {items.map((elem) => {
                         return (
-                            <AnimatePresence>
                             <motion.div
                                 className="eachItem"
                                 key={elem.id}
                                 initial={{ x: "100vw" }}
                                 animate={{ x: 0 }}
                                 // transition={{type:'spring', stiffness: 150}}
-                                exit={{ x: "-100vw", scale: [1,0]}}
+                                exit={{ x: "-100vw", scale: [1, 0] }}
                             >
                                 <h3>{elem.name}</h3>
                                 <div className="todo-btn">
@@ -30,25 +28,24 @@ const ShowItem = ({ items, editItem, deleteItem }) => {
                                         className="far fa-edit add-btn"
                                         title="Edit Item"
                                         onClick={() => editItem(elem.id)}
-                                        whileHover={{scale: 1.07}}
-                                        whileTap={{scale: 0.8}}
-                                        transition={{yoyo: Infinity}}
-                                        ></motion.i>
+                                        whileHover={{ scale: 1.07 }}
+                                        whileTap={{ scale: 0.8 }}
+                                        transition={{ yoyo: Infinity }}
+                                    />
                                     <motion.i
                                         className="far fa-trash-alt add-btn"
                                         title="Delete Item"
                                         onClick={() => deleteItem(elem.id)}
-                                        whileHover={{scale: 1.07}}
-                                        whileTap={{scale: 0.8}}
-                                        transition={{yoyo: Infinity}}
-                                    ></motion.i>
+                                        whileHover={{ scale: 1.07 }}
+                                        whileTap={{ scale: 0.8 }}
+                                        transition={{ yoyo: Infinity }}
+                                    />
                                 </div>
                             </motion.div>
-                            </AnimatePresence>
                         );
                     })}
-                </motion.div>
-            </AnimatePresence>
+                </AnimatePresence>
+            </motion.div>
         </>
     );
 };
